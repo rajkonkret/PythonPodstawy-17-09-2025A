3b2efc11ff5cae7edf9aeda79ca4ab4002691207
rajkonkret <rajkonkret660@gmail.com>
2025-09-18 15:38:39 +0200

    [2 22] update discount.py

--- day2/discount.py
+++ day2/discount.py
@@ -34,3 +34,48 @@ print(formated_time_usa)  # 03:16 PM
 object_time = datetime.now().strptime("18/09/2025", "%d/%m/%Y")
 print(object_time)  # 2025-09-18 00:00:00
 print(type(object_time))  # <class 'datetime.datetime'>
+
+print(35 * "-")
+
+products = [
+    {"sku": 1, "exp_date": today, "price": 200},
+    {"sku": 2, "exp_date": today, "price": 100},
+    {"sku": 3, "exp_date": tomorrow, "price": 500},
+    {"sku": 4, "exp_date": today, "price": 80},
+    {"sku": 5, "exp_date": today, "price": 99.99},
+]
+
+for p in products:
+    # print(p)  # {'sku': 5, 'exp_date': datetime.date(2025, 9, 18), 'price': 99.99}
+    # print(p['exp_date'])  # 2025-09-18
+    # if p['exp_date'] == today:
+    #     p['price'] *= 0.8
+    if p['exp_date'] != today:
+        continue
+    p['price'] *= 0.8
+    print("Zmiana ceny")
+    print(f"""
+Price for {p['sku']}
+is now {p['price']:.2f}""")
+# -----------------------------------
+# Zmiana ceny
+#
+# Price for 1
+# is now 160.00
+# Zmiana ceny
+#
+# Price for 2
+# is now 80.00
+# Zmiana ceny
+#
+# Price for 4
+# is now 64.00
+# Zmiana ceny
+#
+# Price for 5
+# is now 79.99
+
+"""fgfg
+gfgffhfgh"""
+'''
+dsfsdfsdfsd'''
