3edf5520485ad45ce654e2c4de73f38c0983b9f3
rajkonkret <rajkonkret660@gmail.com>
2025-09-18 14:26:57 +0200

    [2 17] add petle_zad2.py

---
+++ day2/petle_zad2.py
@@ -0,0 +1,35 @@
+dictionary = {'imie': "Radek", "nazwisko": "Kowalski"}
+print(type(dictionary))  # <class 'dict'>
+
+# wypisze klucze
+for i in dictionary:
+    print(i)
+# imie
+# nazwisko
+
+for i in dictionary.keys():
+    print(i)
+# imie
+# nazwisko
+
+# wypisze wartości
+for v in dictionary.values():
+    print(v)
+# Radek
+# Kowalski
+
+# wypisanie par
+for i in dictionary.items():
+    print(i)
+# ('imie', 'Radek')
+# ('nazwisko', 'Kowalski')
+
+for k, v in dictionary.items():
+    print(k, "=>", v)
+# imie => Radek
+# nazwisko => Kowalski
+
+for k, v in dictionary.items():
+    print(k, v, sep="<++>")
+# imie<++>Radek
+# nazwisko<++>Kowalski
