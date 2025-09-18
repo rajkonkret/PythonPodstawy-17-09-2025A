cd65d9cdf1d4b2fd476572cdc53083ad115f14af
rajkonkret <rajkonkret660@gmail.com>
2025-09-18 14:51:06 +0200

    [2 19] update petle_zad3.py

--- day2/petle_zad3.py
+++ day2/petle_zad3.py
@@ -16,3 +16,28 @@ licznik = 0
 while licznik < 10:
     licznik += 1
     print("Komunikat 3 !!!")
+
+# password = input("Podaj hasło")
+# while password != "secret":  # !=  - różne od
+#     password = input("Błędne hasło. Podaj ponownie")
+# print("Hasło prawidłowe")
+# Podaj hasłoddd
+# Błędne hasło. Podaj ponownieasdasdsa
+# Błędne hasło. Podaj ponownieasddasdas
+# Błędne hasło. Podaj ponowniesaDADA
+# Błędne hasło. Podaj ponownie123
+# Błędne hasło. Podaj ponownieSECRET
+# Błędne hasło. Podaj ponowniesecret
+# Hasło prawidłowe
+
+# lista = []
+# lista_int = []
+# while True:
+#     wej = input("Podaj liczbę")
+#     #  A string is numeric if all characters in the string are numeric
+#     if not wej.isnumeric():
+#         break
+#     lista.append(wej)
+#     lista_int.append(int(wej))
+# print(lista)  # ['1', '2', '3', '4', '5', '6']
+# print(lista_int)  # [1, 2, 3, 4, 5, 6]
